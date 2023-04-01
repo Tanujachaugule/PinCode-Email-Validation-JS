d6cf2f3297fd80758c3cb6c21ae2ed5e480aacf3
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-04-01 14:39:50 +0530

    Added: Email has to have . symbol after email server namel

---
+++ UC3Email.js
@@ -0,0 +1,15 @@
+const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]{2,}[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z0-9-]+)$");
+
+function validateEmail(email){
+    if(emailRegex.test(email))
+        console.log(email + " is a valid EMAIL.");
+    else
+        throw email + " is an invalid EMAIL.";
+}
+
+try{
+    validateEmail("bridgelaz@abc.com");   
+    validateEmail("tanujakkk@abcd");  
+}catch(e){
+    console.error(e);
+}
